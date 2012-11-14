4132936db9c8302294c932b4ab78d9ac4ae691b0
Sergiy Melnyk <raydenman@gmail.com>
2012-11-14 19:28:33 +0200

    add mongoose

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,19 @@
 /*
  * GET home page.
  */
+var mongoose = require('mongoose');
+var db = mongoose.createConnection('mongodb://admin:111111@alex.mongohq.com:10065/Sandbox');
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // yay!
+  console.log("mongo opened!");
+});
+
+var schema = mongoose.Schema({ name: 'string' });
+var Cat = db.model('Cat', schema);
 
 exports.index = function(req, res){
+
   res.render('index', { title: 'Express' });
 };
